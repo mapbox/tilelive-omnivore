a7299316294d814e8d569e23665f86be82c21c20
Ryan Clark <ryan.clark.j@gmail.com>
2015-03-10 14:01:32 -0400

    adjust expected mapnik xml

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function Omnivore(uri, callback) {
 
   function getXml(err, metadata) {
     if (err) return callback(err);
-    
+
     // Stopgap while only 8 bit TIFFs are supported
     if (metadata.dstype === 'gdal' && metadata.raster.bands[0].rasterDatatype !== 'Byte') return callback('Only 8 bit TIFFs are supported');
-    
+
     metadata.filepath = filepath;
     var mapnikXml = Omnivore.getXml(metadata);
     new Bridge({ xml: mapnikXml }, setBridge);
